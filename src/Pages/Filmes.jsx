5a3e6f918ddbd5e8aa406b1576912d17d34a69bc
Larisse <larisse.lima2@gmail.com>
2020-11-23 11:01:40 -0300

    Atualiza componente Filmes

--- src/Pages/Filmes.jsx
+++ src/Pages/Filmes.jsx
@@ -1,13 +1,22 @@
 import filmes from '../Services/dados'
+import '../Styles/Pages/filmes.css'
+import Sidebar from '../Components/Sidebar'
 
-const Filmes = () => {
+const ListaInterna = () => {
 
-    return (
-        <>
-            <div id="container">
-
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Sidebar />
+      <div id="container">
+        {filmes.map(filme => {
+          return <div className="card" key={filme.id}>
+            <h3>Título: {filme.nome}</h3>
+            <p>Gênero: {filme.genero}</p>
+            <img src={filme.capa} alt="capa" />
+          </div>
+        })}
+      </div>
+    </>
+  )
 }
-export default Filmes
+export default ListaInterna
