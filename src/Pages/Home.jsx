2f802fc4243ff3897966ebf99bb1e6be588003a2
Larisse <larisse.lima2@gmail.com>
2020-11-23 11:05:21 -0300

    Atualiza componente Home e adiciona css

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,23 @@
+import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import '../Styles/Pages/home.css';
 
-const Home = () => {
-
+function Home() {
     return (
-        <>
-            <h1>Home</h1>
-        </>
-    )
+        <div id="page-landing">
+            <div className="content-wrapper">
+                <main>
+                    <h1>O que eu faço pra relaxar</h1>
+                    <p>Meu modo favorito de procrastinar</p>
+                </main>
+
+                <Link to="/filmes" className="enter-app">
+                    <FiArrowRight size={26} color="rgba(0, 0 , 0, 0.6)" />
+                </Link>
+            </div>
+        </div>
+    );
 }
-export default Home
+
+export default Home;
